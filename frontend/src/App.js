2f2a6111c93bdf479a9f50b12caf6379163741e8
SanjayKH-Git <56336350+SanjayKH-Git@users.noreply.github.com>
2023-07-23 16:10:10 +0000

    local

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [location, setLocation] = useState("");
+  const [responseData, setResponseData] = useState("");
+
+  const handleInputChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // alert("submit");
+
+      const response = await fetch(`http://127.0.0.1:8000/${location}`);
+      console.log(response); // Add this line to inspect the response
+      const data = await response.json();
+      console.debug(data);
+      // alert(data);
+
+      setResponseData(data.message);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter Location:
+            <input
+              type="text"
+              value={location}
+              onChange={handleInputChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <p>{responseData}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- main.py
+++ main.py
@@ -1,42 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+# Allow requests from all origins during development (adjust as needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sanjaykh-git-sturdy-succotash-vrqv95pgv74275g-8000.preview.app.github.dev/Home"],
-    allow_credentials=True,
+    allow_origins=["http://127.0.0.1:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 @app.get("/")
-def root():
-    return {"message": "Welcome to the Food-Price-Clash API"}
+def read_root():
+    return {"message": "Hello World!"}
 
-# @app.get("/api/todo")
-# def get_todo():
-#     return 1
-
-# @app.get("/api/todo{id}")
-# def get_todo_by_id(id):
-#     return 1
-
-# @app.post("/api/todo")
-# def post_todo_by_id(todo):
-#     return 1
-
-
-@app.get("/location")
-def get_location(location: str):
-    # Here you can implement the logic to handle the location data
-    # and return the desired response
-    # print(location)
-    # print(f"Location received: {location}")
-    
-    return {"location": location}
-
-def getNotes():
-    return "Here is some data"
-
+@app.get("/{location}")
+async def say_hi(location: str):
+    print(f"Received location: {location}")
+    response_data = {"message": f"Hi {location}"}
+    return JSONResponse(content=response_data)
